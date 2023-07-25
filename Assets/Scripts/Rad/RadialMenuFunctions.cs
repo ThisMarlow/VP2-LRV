4783721f52028119223fd421c83a9b3a8f50bbaa
Akasha Kessel <akasha.kessel@student.uni-luebeck.de>
2023-07-25 15:11:32 +0200

    added airspace filter logic

--- Assets/Scripts/Rad/RadialMenuFunctions.cs
+++ Assets/Scripts/Rad/RadialMenuFunctions.cs
@@ -6,9 +6,21 @@ public class RadialMenuFunctions : MonoBehaviour
 {
     public RadialMenu homeMenu;
     public RadialMenu filterMenu; 
+    public GameObject airspaceContainer;
+
     public void SetFilter()
     {
         homeMenu.ChangeActive();
         filterMenu.ChangeActive();
+    }
+
+    public void airSpaceChange() {
+        if (airspaceContainer.activeSelf == true){
+            airspaceContainer.SetActive(false);
+        }
+        else if (airspaceContainer.activeSelf != true)
+        {
+            airspaceContainer.SetActive(true);
+        }
     }
 }
