a0ab50b7214265b3a010d97a958ed72ac3a8f6d8
Philipp0403 <philipp.uesbeck@student.uni-luebeck.de>
2023-07-25 15:36:43 +0200

    update functions

--- Assets/Scripts/Rad/RadialMenuFunctions.cs
+++ Assets/Scripts/Rad/RadialMenuFunctions.cs
@@ -3,24 +3,30 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class RadialMenuFunctions : MonoBehaviour
-{
+{/*
     public RadialMenu homeMenu;
-    public RadialMenu filterMenu; 
+    public RadialMenu filterMenu; */
     public GameObject airspaceContainer;
-
+    /*
     public void SetFilter()
     {
         homeMenu.ChangeActive();
         filterMenu.ChangeActive();
-    }
+    }*/
 
     public void airSpaceChange() {
+        print("Sind in der Methode");
+        Debug.Log("Sind in der Methode");
         if (airspaceContainer.activeSelf == true){
             airspaceContainer.SetActive(false);
+            print("Setz False");
+            Debug.Log("Setz False");
         }
         else if (airspaceContainer.activeSelf != true)
         {
             airspaceContainer.SetActive(true);
+            print("Setz True");
+            Debug.Log("Setz True");
         }
     }
 }
